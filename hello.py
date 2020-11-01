b89e20ea984de33459fe3ed9a79e917e5f76cd4d
Noah Gift <noah.gift@gmail.com>
2020-11-01 20:38:40 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,6 @@
+def add(x, y):
+    return x + y
+
+
+result = add(1, 2)
+print(f"This is the sum: 1, 2, {result}")
---
+++ test_hello.py
@@ -0,0 +1,4 @@
+from hello import add
+
+def test_add():
+    assert add(1,2) == 3
